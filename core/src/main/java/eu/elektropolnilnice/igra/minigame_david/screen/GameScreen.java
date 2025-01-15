6e15ee5e6130cb63f4022e20e1f5cf4c5e122b53
davidgole1 <david.gole1@student.um.si>
2025-01-15 22:21:32 +0100

    dodajanje TileMapa in enostavno premikanje avta

--- core/src/main/java/eu/elektropolnilnice/igra/minigame_david/screen/GameScreen.java
+++ core/src/main/java/eu/elektropolnilnice/igra/minigame_david/screen/GameScreen.java
@@ -1,9 +1,20 @@
 package eu.elektropolnilnice.igra.minigame_david.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -25,16 +36,66 @@ public class GameScreen extends ScreenAdapter {
     private Stage stage;
     private Skin skin;
 
+    private OrthographicCamera camera;
+
+    private float tileWidth;
+    private float tileHeight;
+    private float mapWidthInPx;
+    private float mapHeightInPx;
+
+    private TiledMap tiledMap;
+    private TiledMapTileLayer grassLayer;
+    private MapObjects mapObjects;
+    private OrthogonalTiledMapRenderer tiledMapRenderer;
+
+    private Sound sound;
+    private Texture carTexture;
+    private Sprite player;
+
+    private BitmapFont font;
+
+    private int health;
+    private int score;
+
     public GameScreen() {
         this.assetManager = Main.Instance().getAssetManager();
     }
 
     @Override
     public void show() {
-        viewport = new FitViewport(GameConfig.HUD_WIDTH, GameConfig.HUD_HEIGHT);
+        // Ustvarimo kamero in viewport, ki ohranjata razmerje zemljevida
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(GameConfig.HUD_WIDTH, GameConfig.HUD_HEIGHT, camera);
         stage = new Stage(viewport, Main.Instance().getBatch());
 
         skin = assetManager.get(AssetDescriptors.UI_SKIN);
+
+        // Nalaganje zemljevida in rendererja
+        tiledMap = new TmxMapLoader().load("tiledmap/minigame_road1.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        // Pridobivanje dimenzij zemljevida
+        grassLayer = (TiledMapTileLayer) tiledMap.getLayers().get("ozadje");
+        tileWidth = grassLayer.getTileWidth();
+        tileHeight = grassLayer.getTileHeight();
+        mapWidthInPx = grassLayer.getWidth() * tileWidth;
+        mapHeightInPx = grassLayer.getHeight() * tileHeight;
+
+        // Kamera je postavljena na središče zemljevida
+        camera.setToOrtho(false, viewport.getWorldWidth(), viewport.getWorldHeight());
+        camera.position.set(mapWidthInPx / 2f, mapHeightInPx / 2f, 0);
+        camera.update();
+
+        // Nalaganje teksture in nastavljanje igralca
+        carTexture = new Texture("gameplay/carBlue.png");
+        player = new Sprite(carTexture);
+        player.setScale(0.35f); // Pomanjšamo igralca na 50% originalne velikosti
+        player.setPosition(mapWidthInPx / 2f - player.getWidth() / 2f, 0);
+
+        font = new BitmapFont();
+
+        health = 100;
+        score = 0;
 
         stage.addActor(createUi());
         Gdx.input.setInputProcessor(stage);
@@ -49,8 +110,91 @@ public class GameScreen extends ScreenAdapter {
     public void render(float delta) {
         ScreenUtils.clear(Color.LIGHT_GRAY);
 
+        handleConfigurationInput();
+
+        if (health > 0) {
+            handleGameplayInput();
+            update();
+        }
+
+        // Kamera sledi igralcu
+        camera.position.set(
+            Math.max(camera.viewportWidth / 2f, Math.min(player.getX() + player.getWidth() / 2, mapWidthInPx - camera.viewportWidth / 2f)),
+            Math.max(camera.viewportHeight / 2f, Math.min(player.getY() + player.getHeight() / 2, mapHeightInPx - camera.viewportHeight / 2f)),
+            0
+        );
+        camera.update();
+
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+
+        tiledMapRenderer.getBatch().begin();
+        player.draw(tiledMapRenderer.getBatch());
+        font.draw(tiledMapRenderer.getBatch(), "SCORE: " + score, 20f, camera.position.y + camera.viewportHeight / 2f - 20f);
+        font.draw(tiledMapRenderer.getBatch(), "HEALTH: " + health, 20f, camera.position.y + camera.viewportHeight / 2f - 40f);
+        tiledMapRenderer.getBatch().end();
+
         stage.act(delta);
         stage.draw();
+    }
+
+
+
+    private void handleGameplayInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            player.setX(player.getX() - 100 * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            player.setX(player.getX() + 100 * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            player.setY(player.getY() + 100 * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            player.setY(player.getY() - 100 * Gdx.graphics.getDeltaTime());
+        }
+    }
+
+    private void handleConfigurationInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            camera.translate(-8, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            camera.translate(8, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            camera.translate(0, 8);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            camera.translate(0, -8);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.B)) {
+            camera.position.set(mapWidthInPx / 2f, mapHeightInPx / 2f, 0);
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) {
+            tiledMap.getLayers().get("ozadje").setVisible(!tiledMap.getLayers().get("ozadje").isVisible());
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
+            tiledMap.getLayers().get("cesta").setVisible(!tiledMap.getLayers().get("cesta").isVisible());
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
+    }
+
+    private void update() {
+        // calculate tile coordinates
+        int tileX = (int) ((player.getX() + player.getWidth() / 2) / tileWidth);
+        int tileY = (int) ((player.getY() + player.getHeight() / 2) / tileHeight);
+
+
+        // check collision between player and obstacles
+//        for (MapObject mapObject : mapObjects) {
+//            if (player.getBoundingRectangle().overlaps(((RectangleMapObject) mapObject).getRectangle())) {
+//                sound.play();
+//                health--;
+//            }
+//        }
     }
 
     @Override
@@ -60,6 +204,9 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
+        tiledMap.dispose();
+        carTexture.dispose();
+        font.dispose();
         stage.dispose();
     }
 
