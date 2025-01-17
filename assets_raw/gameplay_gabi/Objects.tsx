b1b82f3aacbc899ffd66ffbd35fd10fceba9b8a8
gabriel zadravec <gabriel.zadravec@student.um.si>
2025-01-17 11:39:29 +0100

    added assets

---
+++ assets_raw/gameplay_gabi/Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-93-ge62b7ff9" name="Objects" tilewidth="64" tileheight="64" tilecount="196" columns="14">
+ <image source="kenney_racing-pack/Spritesheets/spritesheet_objects.png" width="918" height="929"/>
+</tileset>
---
+++ assets_raw/gameplay_gabi/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-93-ge62b7ff9" name="Tiles" tilewidth="64" tileheight="64" tilecount="1296" columns="36">
+ <image source="kenney_racing-pack/Spritesheets/spritesheet_tiles.png" width="2304" height="2304"/>
+</tileset>
