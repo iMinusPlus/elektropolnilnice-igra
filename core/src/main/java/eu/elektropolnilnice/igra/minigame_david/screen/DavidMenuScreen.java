e4aa59153d86bfb599d1c433d6688c195744381d
davidgole1 <david.gole1@student.um.si>
2025-01-18 21:55:51 +0100

    Narejen menuscreen za DavidMinigame

--- core/src/main/java/eu/elektropolnilnice/igra/minigame_david/screen/DavidMenuScreen.java
+++ core/src/main/java/eu/elektropolnilnice/igra/minigame_david/screen/DavidMenuScreen.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -15,6 +17,7 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import eu.elektropolnilnice.igra.GameConfig;
 import eu.elektropolnilnice.igra.Main;
 import eu.elektropolnilnice.igra.assets.AssetDescriptors;
+import eu.elektropolnilnice.igra.assets.RegionNames;
 
 public class DavidMenuScreen extends ScreenAdapter {
 
@@ -23,6 +26,7 @@ public class DavidMenuScreen extends ScreenAdapter {
     private Viewport viewport;
     private Stage stage;
     private Skin skin;
+    private TextureAtlas gameplay;
 
     public DavidMenuScreen() {
         assetManager = Main.Instance().getAssetManager();
@@ -34,10 +38,13 @@ public class DavidMenuScreen extends ScreenAdapter {
         stage = new Stage(viewport, Main.Instance().getBatch());
 
         assetManager.load(AssetDescriptors.UI_SKIN);
+        assetManager.load(AssetDescriptors.DAVID_GAMEPLAY);
         assetManager.finishLoading();
 
         skin = assetManager.get(AssetDescriptors.UI_SKIN);
+        gameplay = assetManager.get(AssetDescriptors.DAVID_GAMEPLAY);
 
+        stage.addActor(createBackground());
         stage.addActor(createUi());
         Gdx.input.setInputProcessor(stage);
     }
@@ -65,12 +72,20 @@ public class DavidMenuScreen extends ScreenAdapter {
         stage.dispose();
     }
 
+    private Actor createBackground() {
+        Image background = new Image(gameplay.findRegion(RegionNames.MENU_BACKGROUND));
+        background.setFillParent(true);
+        return background;
+    }
+
     private Actor createUi() {
         Table table = new Table();
         table.defaults().pad(20);
 
-        Label title = new Label("David's MINIGAME", skin);
+        Image logo = new Image(gameplay.findRegion(RegionNames.LOGO_1));
         TextButton playButton = new TextButton("PLAY", skin);
+        TextButton howToPlayButton = new TextButton("HOW TO PLAY", skin);
+
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -78,9 +93,19 @@ public class DavidMenuScreen extends ScreenAdapter {
             }
         });
 
-        table.add(title).center();
+        howToPlayButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Dodaj funkcionalnost za HOW TO PLAY
+                System.out.println("How to play clicked!");
+            }
+        });
+
+        table.add(logo).center().padBottom(50);
         table.row();
-        table.add(playButton).center();
+        table.add(playButton).center().width(200).height(50);
+        table.row();
+        table.add(howToPlayButton).center().width(200).height(50);
         table.center();
         table.setFillParent(true);
 
